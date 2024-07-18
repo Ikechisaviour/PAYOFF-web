142af8cc2f87fc81d8c1e02c1af47fec62079e8f
berryboylb <phemmynesce4life@gmail.com>
2024-07-18 12:18:58 +0100

    @chore: png

---
+++ assets/pngs/index.ts
@@ -0,0 +1,41 @@
+import Testiomonial1 from "./Ellipse 1.png";
+import Testiomonial2 from "./Ellipse 1 (1).png";
+import Testiomonial3 from "./Ellipse 1 (2).png";
+import Testiomonial4 from "./Ellipse 1 (3).png";
+import Testiomonial5 from "./Ellipse 1 (4).png";
+import Testiomonial6 from "./Ellipse 1 (5).png";
+import Testiomonial7 from "./Ellipse 1 (6).png";
+import Testiomonial8 from "./Ellipse 1 (7).png";
+import Testiomonial9 from "./Ellipse 1 (8).png";
+import Cards from "./Group 1000002408.png";
+import Logo from "./Payoff-No-Background.png";
+import Ndic from "./ndic-Logo-1024x433-1 1.png";
+import CentralBank from "./central-bank-of-nigeria-cbn-logo-42FD3093EE-seeklogo.com 1.png";
+// import WhiteLogo from "./Frame 75.png";
+import Carousel1 from "./Rectangle 7.png";
+import Carousel2 from "./Rectangle 8.png";
+import Carousel3 from "./Rectangle 9.png";
+import Carousel4 from "./Rectangle 10.png";
+// import Bag from "./bag.png";
+
+export {
+  // Bag,
+  Testiomonial1,
+  Testiomonial2,
+  Testiomonial3,
+  Testiomonial4,
+  Testiomonial5,
+  Testiomonial6,
+  Testiomonial7,
+  Testiomonial8,
+  Testiomonial9,
+  Cards,
+  Logo,
+  Ndic,
+  CentralBank,
+  // WhiteLogo,
+  Carousel1,
+  Carousel2,
+  Carousel3,
+  Carousel4,
+};
