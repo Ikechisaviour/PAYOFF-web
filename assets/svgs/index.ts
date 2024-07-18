5f2778be973b430b4b801c102fbb73381afafea5
berryboylb <phemmynesce4life@gmail.com>
2024-07-18 12:31:17 +0100

    @chore: fix

---
+++ assets/svgs/index.ts
@@ -0,0 +1,39 @@
+// import Logo from "./logosaas.svg";
+import HeroImg from './hero.svg';
+import IconButton from './Icon Button.svg'
+import Line from './line.svg';
+import Ellipse2 from './Ellipse 2.svg';
+import Ellipse3 from './Ellipse 3.svg';
+import Ellipse4 from './Ellipse 4.svg';
+import Ellipse5 from './Ellipse 5.svg';
+import Ellipse6 from './Ellipse 6.svg';
+
+import PhoneDashboard from "./phinead.svg";
+import PhoneUpdates from "./div.Phone__PhoneContainer-sc-3jzr8d-1.svg";
+import FastAndSecure from "./fastandSecure.svg";
+import RealTimeUpdates from "./realtimeUpdates.svg";
+import BillPayments from "./billPayments.svg";
+
+import AuthImage from './Section.svg';
+import Bag from './bag.svg'
+import WhiteLogo from "./whitelogo.svg"
+
+export {
+  // Logo,
+  HeroImg,
+  IconButton,
+  Line,
+  Ellipse2,
+  Ellipse3,
+  Ellipse4,
+  Ellipse5,
+  Ellipse6,
+  AuthImage,
+  PhoneDashboard,
+  PhoneUpdates,
+  FastAndSecure,
+  RealTimeUpdates,
+  BillPayments,
+  Bag,
+  WhiteLogo,
+};
