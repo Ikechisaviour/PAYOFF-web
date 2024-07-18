0798bbe70e9d6709aeb1cea86b5740712116ef96
berryboylb <phemmynesce4life@gmail.com>
2024-07-18 12:03:07 +0100

    @chore: lib

---
+++ lib/constants.ts
@@ -0,0 +1,100 @@
+import * as  dateFns from "date-fns";
+
+// Api Endpoints
+export const ENDPOINTS = {
+  API_BASE_DEV_URL: "https://api-staging-natep.innox.africa/api",
+  API_SIGN_UP: "/auth/signup",
+  API_LOGIN: "/auth/admin/signin",
+  API_FORGOT: "/admin/password-reset",
+  API_VERIFY_FORGOT: "/admin/verify-password-reset",
+  API_UPDATE_PASSWORD: "/admin/update-password",
+  API_USER_APPLICATION: "/applicant/get-user-application",
+  API_START_APPLICATION: "/applicant/start-application",
+  API_RESEND_EMAIL_APPLICATION: "/applicant/resend-email-verification",
+  API_VERIFY_TOKEN: "/applicant/verify-token",
+  API_UPDATE_PERSONAL_INFO: "/applicant/update-personal-info",
+  API_CONTACT_INFO: "/applicant/update-contact-info",
+  API_ENROLLMENT_INFO: "/applicant/update-enrolment-info",
+  API_IMAGES_INFO: "/applicant/update-application-images",
+  API_SUBMIT_INFO: "/applicant/submit-application",
+  API_CAPTURE_APPOINTMENT: "/applicant/set-capture-appointment",
+  API_FILE_UPLOAD: "/applicant/file-upload",
+  API_ADD_ADMIN: "/admin/add-admin",
+  API_GET_ADMIN_DASHBOARD: "/admin/get-admin-dashboard",
+  API_GET_APPLICATIONS: "/admin/get-applications",
+  API_GET_SINGLE_APPLICATIONS: (id: string) => `/admin/get-application/${id}`,
+  API_GET_SINGLE_APPOINTMENT: (id: string) => `/admin/get-appointment/${id}`,
+  API_GET_APPOINTMENTS: "/admin/get-appointments",
+  API_GET_CALENDAR: "/admin/get-calendar",
+  API_SET_SCHEDULE: "/admin/set-available-schedule",
+  API_GENERATE_PAYMENT: (type: string) =>
+    `/applicant/generate-payment-link/${type}`,
+  API_GENERATE_CERTIFICATE: (id: string) => `/admin/generate-certificate/${id}`,
+  API_PREVIEW_CERTIFICATE: (id: string) => `/admin/preview-certificate/${id}`,
+  API_SEND_CERTIFICATE: (id: string) => `/admin/send-certificate/${id}`,
+  CLOUDINARY_URL: (cloudname: string) =>
+    `https://api.cloudinary.com/v1_1/${cloudname}/auto/upload`,
+};
+
+export const axiosConfig = {
+  headers: { "Content-Type": "application/json" },
+};
+
+export const NAMESPACE = {
+  // User endpoints
+  API_USER_APPLICATION: "API_USER_APPLICATION",
+  API_GET_ADMIN_DASHBOARD: " API_GET_ADMIN_DASHBOARD",
+  API_GET_APPLICATIONS: "API_GET_APPLICATIONS",
+  API_GET_SINGLE_APPLICATIONS: (id: string) =>
+    `API_GET_SINGLE_APPLICATIONS_${id}`,
+  API_GET_APPOINTMENTS: "API_GET_APPOINTMENTS",
+  API_GET_CALENDAR: "API_GET_CALENDAR",
+  API_GET_SINGLE_APPOINTMENT: (id: string) =>
+    `API_GET_SINGLE_APPOINTMENT_${id}`,
+  API_GENERATE_CERTIFICATE: (id: string) => `API_GENERATE_CERTIFICATE_${id}`,
+  API_PREVIEW_CERTIFICATE: (id: string) => `API_PREVIEW_CERTIFICATE_${id}`,
+};
+
+interface IndexableObject {
+  [key: string]: any;
+}
+
+export const APP_KEYS = {
+  TOKEN: "token",
+  MAX_SIZE_IN_BYTES: 10 * 1024 * 1024, //  10MB
+  ALLOWED_MEDIA_TYPES: [
+    "image/svg+xml",
+    "image/png",
+    "image/jpeg",
+    "image/gif",
+    "application/pdf",
+  ],
+  MEDIA_DEFAULTS: {
+    icon: "cloudUpload",
+    photo: "",
+    percentage: 0,
+    completed: 0,
+    uploading: false,
+    name: "",
+    pending: false,
+  },
+  LAST_PAGE: "LAST_PAGE",
+  ACCESS_TOKEN: "ACCESS_TOKEN",
+  REFRESH_TOKEN: "REFRESH_TOKEN",
+  USER: "USER",
+  PROTECTED_ROUTES: ["dashboard", "onboard", "review"],
+  SUCCESSFULLY_VERIFIED_NIN: "SUCCESSFULLY_VERIFIED_NIN",
+  OTP_DIALOGUE: "OTP_DIALOGUE",
+  NIN_VERIFIED: "NIN_VERIFIED",
+  PERSONAL_INFO: "PERSONAL_INFO",
+  TRANSACTION_TYPES: [
+    { label: "All", value: "all" },
+    { label: "Direct Debits", value: "debit" },
+    { label: "Credits", value: "credit" },
+  ],
+  DATE_TYPES: [
+    { label: "Today", value: new Date() },
+    { label: "Yesterday", value: dateFns.subDays(new Date(), 1) },
+    { label: "Last week", value: dateFns.subDays(new Date(), 7) },
+  ],
+};
---
+++ lib/utils.ts
@@ -0,0 +1,145 @@
+import { type ClassValue, clsx } from "clsx"
+import { twMerge } from "tailwind-merge"
+import axios from "axios"
+import Cookies from "js-cookie";
+import { APP_KEYS  } from './constants';
+
+
+const cookieConfig: any = {
+  expires: 7, //expires in 7day
+  secure: true, //when all is done change to true
+  sameSite: "Lax", //set to 'strict' when all is done
+  path: "/",
+};
+
+export  const setCookie = (key: string, value: string) =>
+  Cookies.set(key, value, cookieConfig);
+export  const removeCookie = (key: string) =>
+  Cookies.remove(key);
+
+export  const getCookie = (key: string) =>
+    Cookies.get(key);
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+export function returnError(error: unknown): string {
+  console.error(error);
+
+  if (axios.isAxiosError(error)) 
+    return error.response?.data?.message ?? 'An error occurred';
+  
+  
+  if (error instanceof Error) 
+    return error.message;
+  
+  
+  return 'An unknown error occurred';
+}
+
+export function isRouteProtected(currentRoute: string) {
+  return APP_KEYS.PROTECTED_ROUTES.some(route => currentRoute.includes(route));
+}
+
+export function setToken(token:string) {
+  axios.defaults.headers.common["Authorization"] = `Bearer ${token}`; 
+}
+
+export function userLogout() {
+  delete axios.defaults.headers.common['Authorization'];
+  removeCookie(APP_KEYS.ACCESS_TOKEN);
+  removeCookie(APP_KEYS.REFRESH_TOKEN);
+  sessionStorage.removeItem(APP_KEYS.USER)
+}
+
+
+export const getItemFromLocalStorage = (key: string): boolean => {
+  if (typeof window === "undefined") return false; // Ensure localStorage is accessed only in the browser
+  const item = localStorage.getItem(key);
+  return item ? JSON.parse(item) : false;
+};
+
+export const setItemInLocalStorage = (key: string, value: boolean): void => {
+  if (typeof window !== "undefined") {
+    localStorage.setItem(key, JSON.stringify(value));
+  }
+};
+
+export function convertToISOString(dateString: string) {
+    if(!dateString)  return '0000-00-00'
+    const [day, month, year] = dateString?.split('-').map(part => parseInt(part, 10));
+    const date = new Date(Date.UTC(year, month - 1, day));
+    const datePart = date.toISOString().split('T')[0];
+    return datePart;
+}
+
+export function combineDateAndTime(dateString: Date, timeString: string) {
+    const newDate = new Date(dateString.getTime());
+
+    // Parse the time string into hours and minutes
+    const [hours, minutes] = timeString.split(':').map(part => parseInt(part, 10));
+    newDate.setUTCHours(hours);
+    newDate.setUTCMinutes(minutes);
+    newDate.setUTCSeconds(0);
+    newDate.setUTCMilliseconds(0);
+    return newDate.toISOString();
+}
+
+export function formatDateToYYYYMMDD(date: Date) {
+    const year = date.getFullYear();
+    const month = String(date.getMonth() + 1).padStart(2, '0'); // getMonth() returns 0-11, so add 1
+    const day = String(date.getDate()).padStart(2, '0');
+    return `${year}-${month}-${day}`;
+}
+
+export function calculateAge(dateString: string) {
+    // Parse the date string to extract the year, month, and day
+    const [year, month, day] = dateString.split('-').map(part => parseInt(part, 10));
+
+    // Create a Date object for the birthdate
+    const birthDate = new Date(year, month - 1, day); // Month is zero-based
+
+    // Get the current date
+    const today = new Date();
+
+    // Calculate age
+    let age = today.getFullYear() - birthDate.getFullYear();
+    const monthDiff = today.getMonth() - birthDate.getMonth();
+    const dayDiff = today.getDate() - birthDate.getDate();
+
+    // Adjust age if birthdate hasn't occurred yet this year
+    if (monthDiff < 0 || (monthDiff === 0 && dayDiff < 0)) {
+        age--;
+    }
+
+    return age;
+}
+
+
+
+export function formatTableDate(isoString: string) {
+  if (!isoString) return;
+    const date = new Date(isoString);
+
+    // Month names
+    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    
+    // Get parts of the date
+    const month = monthNames[date.getMonth()];
+    const day = date.getDate();
+    const year = date.getFullYear();
+    
+    // Get hours and minutes
+    let hours = date.getHours();
+    const minutes = date.getMinutes();
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    
+    hours = hours % 12;
+    hours = hours ? hours : 12; // the hour '0' should be '12'
+    const minutesStr = minutes < 10 ? '0' + minutes : minutes;
+
+    // Construct the formatted date string
+    const formattedDate = `${month} ${day}, ${year} | ${hours}:${minutesStr}${ampm}`;
+
+    return formattedDate;
+}
