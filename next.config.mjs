082cb9c134a7ac13258db4037bb708a9cb2f7cbd
berryboylb <phemmynesce4life@gmail.com>
2024-07-18 12:15:06 +0100

    @chore:config

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["via.placeholder.com", "natep.s3.eu-west-2.amazonaws.com"],
+  },
+};
+
+export default nextConfig;
