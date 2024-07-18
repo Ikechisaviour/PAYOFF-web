26763d4a12957b39ce1726cf6863d71d7e44eb7a
berryboylb <phemmynesce4life@gmail.com>
2024-07-18 12:05:39 +0100

    @chore: types

---
+++ types/index.d.ts
@@ -0,0 +1,70 @@
+import type { Icon } from "lucide-react";
+import { Icons } from "@/components/icons";
+import { StaticImageData } from "next/image";
+
+export type Testimonial = {
+  content: string;
+  name: string;
+  avatar: StaticImageData;
+  username: string;
+};
+
+export type KeyPoints = {
+  title: string;
+  description: string;
+  icon: keyof typeof Icons;
+  href: string;
+};
+
+export type NavItem = {
+  title: string;
+  href: string;
+  disabled?: boolean;
+};
+
+export type Footer = {
+  header: string;
+  links: Array<NavItem & { important?: boolean }>;
+};
+
+export type MainNavItem = NavItem;
+
+export type SidebarNavItem = {
+  title: string;
+  disabled?: boolean;
+  external?: boolean;
+  icon?: keyof typeof Icons;
+} & (
+  | {
+      href: string;
+      items?: never;
+    }
+  | {
+      href?: string;
+      items: NavLink[];
+    }
+);
+
+export type DashboardConfig = {
+  landingNav: Array<MainNavItem & { unavailable?: boolean }>;
+  mainNav: MainNavItem[];
+  sidebarNav: SidebarNavItem[];
+  bottomNav: SidebarNavItem[];
+};
+
+export type SiteConfig = {
+  name: string;
+  description: string;
+  url: string;
+  ogImage: string;
+  links: {
+    twitter: string;
+    github: string;
+  };
+};
+
+export type ISortTable = {
+  transaction: string;
+  date: Array<Date>;
+  dateLabel: Array<string>;
+};
